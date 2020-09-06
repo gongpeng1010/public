a2f4ab9e6c2735696ecbc4311f51aa037e98ea0f
gongpeng <1457098464@qq.com>
2020-09-06 23:35:45 +0800

    test

--- src/pages/errorPage/404.vue
+++ src/pages/errorPage/404.vue
@@ -2,7 +2,7 @@
     <div style="background:#f0f2f5;margin-top: -20px;height:100%;">
         <div class="wscn-http404">
             <div class="pic-404"></div>
-            <div class="bullshit">
+            <div class="bullshit">  
                 <div class="bullshit__headline">{{ message }}</div>
                 <div class="bullshit__info">请检查您输入的网址是否正确</div>
                 <a @click="backToHome" class="bullshit__return-home">返回首页</a>
